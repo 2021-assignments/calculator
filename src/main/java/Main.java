e9a93f11b3ba2808b8a4d3a44a142721ae715120
junghyun445 <lknm445@naver.com>
2021-11-27 10:45:30 +0900

    feat : 무한루프

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,23 +10,33 @@ public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        try {
-            System.out.print("enter formula\n>> ");
-            String input = br.readLine();
+        while (true)
+        {
+            try {
+                System.out.println("If you want to end, enter \"exit\".");
+                System.out.print("enter formula\n>> ");
+                String input = br.readLine();
+                if(input.equals("exit"))
+                {
+                    System.out.println("bye");
+                    break;
+                }
 
-            String[] split = input.split(" ");
+                String[] split = input.split(" ");
 
-            if (split.length != 3) {
+                if (split.length != 3) {
+                    throw new IllegalUserInputException("-_-");
+                }
+                long operand1 = Long.parseLong(split[0]);
+                long operand2 = Long.parseLong(split[2]);
+                Operand operand = new Operand(split[1]);
+
+                System.out.println("result : " + operand.calculate(operand1, operand2));
+
+            } catch (NumberFormatException e) {
                 throw new IllegalUserInputException("-_-");
             }
-            long operand1 = Long.parseLong(split[0]);
-            long operand2 = Long.parseLong(split[2]);
-            Operand operand = new Operand(split[1]);
 
-            System.out.println("result : " + operand.calculate(operand1, operand2));
-
-        } catch (NumberFormatException e) {
-            throw new IllegalUserInputException("-_-");
-        }
     }
+}
 }
